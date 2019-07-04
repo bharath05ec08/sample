4106f5ea055558ace25669bb333202defdd120f7
bharath05ec08 <bharath05ec08@gmail.com>
2019-07-04 20:29:39 +0530

    Explored API to get the projects through the code

---
+++ interview_assignment/src/main/java/com/slate/checking/TodoistAPI.java
@@ -0,0 +1,27 @@
+package com.slate.checking;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.entity.mime.Header;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TodoistAPI {
+
+	
+	public static void main(String args[]) throws Exception
+	{
+		RequestSpecification req=RestAssured.given();
+		Header auth=new Header();
+		;
+	req.header(new io.restassured.http.Header("Authorization","Bearer 5218e851ebb0f561785972e5244d5112b97eb7a0"));
+	
+		URI fromSession=new URI("https://beta.todoist.com/API/v8/projects");
+		Response res=req.get(fromSession);
+		
+		System.out.println("Response is "+res.asString());
+	}
+}
