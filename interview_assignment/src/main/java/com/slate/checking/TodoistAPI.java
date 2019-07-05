8fdf527aa7b2b1cdd0a51a5270a67a941494be0d
bharath05ec08 <bharath05ec08@gmail.com>
2019-07-05 06:54:56 +0530

    Changed the rest assured version as it is failing to change the content type. Added the method to create the project through API.

--- interview_assignment/src/main/java/com/slate/checking/TodoistAPI.java
+++ interview_assignment/src/main/java/com/slate/checking/TodoistAPI.java
@@ -3,11 +3,13 @@ package com.slate.checking;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.http.entity.mime.Header;
+import com.google.gson.JsonObject;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 
 /*
  * Author Bharath Mohankumar
@@ -17,17 +19,43 @@ import io.restassured.specification.RequestSpecification;
 
 public class TodoistAPI {
 
+	private static RequestSpecification REQ=RestAssured.given();
+	private static Header AUTH=new Header("Authorization","Bearer 5218e851ebb0f561785972e5244d5112b97eb7a0");
+	private static Header CONTENT=new Header("Content-Type","application/json");
+	private static String ENDPOINT="https://beta.todoist.com/API/v8/";
+	private static URI URL_ENDPOINT;
+	private static Response RES;
+	private static String REQ_DATA1="{\"name\": \"";
+	private static String REQ_DATA2="\"}";
 	
 	public static void main(String args[]) throws Exception
 	{
-		RequestSpecification req=RestAssured.given();
-		Header auth=new Header();
-		;
-	req.header(new io.restassured.http.Header("Authorization","Bearer 5218e851ebb0f561785972e5244d5112b97eb7a0"));
-	
-		URI fromSession=new URI("https://beta.todoist.com/API/v8/projects");
-		Response res=req.get(fromSession);
+		REQ.header(AUTH);	
+		URL_ENDPOINT = new URI(ENDPOINT+"projects");
+		RES = REQ.get(URL_ENDPOINT);
 		
-		System.out.println("Response is "+res.asString());
+		System.out.println("Response is "+RES.asString());
+		RestAssured.baseURI="https://beta.todoist.com/API/v8/";
+		createProject("Project_bharath1");
+	}
+	
+	
+	public static String createProject(String projectName) throws URISyntaxException
+	{RestAssured.baseURI="https://beta.todoist.com/API/v8/";
+		JsonObject loginCredentials = new JsonObject();
+        loginCredentials.addProperty("name", "PROJECT1");
+        REQ=RestAssured.given().contentType("").contentType("Content-Type,application/json").contentType(ContentType.JSON);
+        System.out.println(loginCredentials.toString());
+        
+		REQ.header(AUTH).header(CONTENT).body(REQ_DATA1+projectName+REQ_DATA2);
+		REQ.header(AUTH).header(CONTENT).body("{\r\n" + 
+				"    \"name\": \"PROJECT2\"\r\n" + 
+				"}");
+		
+		URL_ENDPOINT = new URI(ENDPOINT+"projects");
+		RES = REQ.post(URL_ENDPOINT).peek();
+		
+		System.out.println("Project is "+RES.asString()+"\n status code"+RES.statusCode());
+		return "";
 	}
 }
