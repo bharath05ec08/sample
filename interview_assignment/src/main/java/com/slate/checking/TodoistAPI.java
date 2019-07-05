5ef80fc0f6f6453a0f6756430332c50a60c0bd35
bharath05ec08 <bharath05ec08@gmail.com>
2019-07-05 06:54:56 +0530

    Changed the rest assured version as it is failing to change the content type. Added the method to create the project through API.

--- interview_assignment/src/main/java/com/slate/checking/TodoistAPI.java
+++ interview_assignment/src/main/java/com/slate/checking/TodoistAPI.java
@@ -3,11 +3,12 @@ package com.slate.checking;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.http.entity.mime.Header;
+import com.google.gson.JsonObject;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.http.Header;
 
 /*
  * Author Bharath Mohankumar
@@ -17,17 +18,38 @@ import io.restassured.specification.RequestSpecification;
 
 public class TodoistAPI {
 
+	private static RequestSpecification REQ=RestAssured.given();
+	private static Header AUTH=new Header("Authorization","Bearer 5218e851ebb0f561785972e5244d5112b97eb7a0");
+	private static Header CONTENT=new Header("Content-Type","application/json");
+	private static String ENDPOINT="https://beta.todoist.com/API/v8/";
+	private static URI URL_ENDPOINT;
+	private static Response RES;
 	
 	public static void main(String args[]) throws Exception
 	{
-		RequestSpecification req=RestAssured.given();
-		Header auth=new Header();
-		;
-	req.header(new io.restassured.http.Header("Authorization","Bearer 5218e851ebb0f561785972e5244d5112b97eb7a0"));
-	
-		URI fromSession=new URI("https://beta.todoist.com/API/v8/projects");
-		Response res=req.get(fromSession);
+		REQ.header(AUTH);	
+		URL_ENDPOINT = new URI(ENDPOINT+"projects");
+		RES = REQ.get(URL_ENDPOINT);
 		
-		System.out.println("Response is "+res.asString());
+		System.out.println("Response is "+RES.asString());
+		RestAssured.baseURI="https://beta.todoist.com/API/v8/";
+		createProject("Project_bharath1");
+	}
+	
+	
+	public static void createProject(String projectName) throws URISyntaxException
+	{
+		RestAssured.baseURI="https://beta.todoist.com/API/v8/";
+		URL_ENDPOINT = new URI(ENDPOINT+"projects");
+		JsonObject PROJECT_NAME = new JsonObject();
+		PROJECT_NAME.addProperty("name", projectName);  
+		 
+		/*There is an issue with Rest assured for post request
+		This is the reference for older version. How ever the same issue is exists in newer version as well
+		 I will work on this issue at the time of closing the project.
+		https://groups.google.com/forum/#!topic/rest-assured/WKkOdjrboSA
+		*/
+		REQ.header(AUTH).header(CONTENT).body(PROJECT_NAME.toString());		
+		RES = REQ.post(URL_ENDPOINT).peek();
 	}
 }
